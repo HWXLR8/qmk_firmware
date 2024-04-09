2e07be8d0457c69856b3359e0b4a9003f38514fe
HWXLR8 <68k@segv.moe>
2024-04-08 20:30:32 -0400

    [niubkb] add qwerty keymap

--- keyboards/niubkb/keymaps/default/keymap.c
+++ keyboards/niubkb/keymaps/default/keymap.c
@@ -10,9 +10,9 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QW] = KEYMAP( /* Qwerty */
-  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                                               KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN ,
-  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,                                               KC_H,    KC_N,    KC_E,    KC_I,    KC_O    ,
-  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   MT(MOD_LCTL, KC_TAB), MT(MOD_LALT, KC_ENT), KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
+  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P ,
+  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
+  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   MT(MOD_LCTL, KC_TAB), MT(MOD_LALT, KC_ENT), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
   KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,                                             KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT  ),
 /*
  *  !       @     up     {    }        ||     pgup    7     8     9    *
